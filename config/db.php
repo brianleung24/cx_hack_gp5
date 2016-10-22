985d4469f00b7ced317a56fe8706ab577b66a957
Kin Ngai Leung <eebrianleung@gmail.com>
2016-10-22 16:37:41 +0800

    Project init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=172.17.0.2;dbname=hackaton',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
---
+++ controllers/SiteController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\db\Query;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex(){
+        return $this->render('index');
+    }
+
+    public function actionNews(){
+        return $this->render('news');
+    }
+
+
+    public function actionCheckIn(){
+        return $this->render('check_in');
+
+    }
+
+    public function actionBoarding(){
+        return $this->render('boarding');
+
+    }
+
+    public function actionOrder(){
+        return $this->render('order');
+
+    }
+
+    public function actionTestdb(){
+        $test = (new Query())
+            ->select('*')
+            ->from('test')
+            ->all();
+
+        return json_encode($test);
+        
+    }
+
+}
