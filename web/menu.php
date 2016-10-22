5d47335e740263ec45ecc1a340d9d17c0811c818
kab <cftanna@hotmail.com>
2016-10-22 21:14:40 +0800

    header, footer, menu
    header, footer, menu

---
+++ web/menu.php
@@ -0,0 +1,93 @@
+<?php include 'header.php';?>
+    <section id="page-breadcrumb">
+        <div class="vertical-center sun">
+             <div class="container">
+                <div class="row">
+                    <div class="action">
+                        <div class="col-sm-12">
+                            <h1 class="title">Portfolio</h1>
+                            <p>Be Creative</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </section>
+    <!--/#action-->
+
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                
+                <div class="portfolio-items">
+                    <div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">
+                        <div class="portfolio-wrapper">
+                            <div class="portfolio-single">
+                                <div class="portfolio-thumb">
+                                    <img src="images/portfolio/1.jpg" class="img-responsive" alt="">
+                                </div>
+                                <div class="portfolio-view">
+                                    <a href="food.php"><img src="images/portfolio/2.jpg" class="img-responsive" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded folio">
+                        <div class="portfolio-wrapper">
+                            <div class="portfolio-single">
+                                <div class="portfolio-thumb">
+                                    <img src="images/portfolio/2.jpg" class="img-responsive" alt="">
+                                </div>
+                                <div class="portfolio-view">
+                                    <a href="food.php"><img src="images/portfolio/2.jpg" class="img-responsive" alt=""></a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-4 col-md-3 portfolio-item design logos">
+                        <div class="portfolio-wrapper">
+                            <div class="portfolio-single">
+                                <div class="portfolio-thumb">
+                                    <img src="images/portfolio/3.jpg" class="img-responsive" alt="">
+                                </div>
+                                <div class="portfolio-view">
+                                    <a href="food.php"><img src="images/portfolio/2.jpg" class="img-responsive" alt=""></a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-4 col-md-3 portfolio-item design logos">
+                        <div class="portfolio-wrapper">
+                            <div class="portfolio-single">
+                                <div class="portfolio-thumb">
+                                    <img src="images/portfolio/4.jpg" class="img-responsive" alt="">
+                                </div>
+                                <div class="portfolio-view">
+                                    <a href="food.php"><img src="images/portfolio/2.jpg" class="img-responsive" alt=""></a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!--/#portfolio-->
+
+
+
+<?php include 'footer.php';?>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="js/lightbox.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>    
+</body>
+</html>
