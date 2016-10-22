8f33ff046a4f559894b106c19e7b2e946c49aa14
Kin Ngai Leung <eebrianleung@gmail.com>
2016-10-22 18:00:43 +0800

    Switch to php without yin framework

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=172.17.0.2;dbname=hackaton',
-    'username' => 'root',
-    'password' => 'password',
-    'charset' => 'utf8',
-];
